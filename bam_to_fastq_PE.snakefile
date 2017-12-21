d6dafcb0916560af83f18265ba061a12e6f3363e
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-12-21 16:11:30 +0200

    Convert TwinsUK data back to fastq

---
+++ bam_to_fastq_PE.snakefile
@@ -0,0 +1,35 @@
+#Convert BAM files to fastq
+rule bam_to_fastq:
+	input:
+		"processed/{dataset}/input_bam/{sample}.bam"
+	output:
+		fq1 = "processed/{dataset}/fastq/{sample}_1.fastq.gz",
+		fq2 = "processed/{dataset}/fastq/{sample}_1.fastq.gz",
+	params:
+		local_temp = "/tmp/" + uuid.uuid4().hex + "/"
+	resources:
+		mem = 1000
+	threads: 1
+	shell:
+		"""
+		module load samtools-1.6
+		mkdir {params.local_tmp}
+		cp {input} {params.local_tmp}/{sample}.bam
+		samtools {params.local_tmp}/{sample}.bam {params.local_tmp}/{sample}.collated
+		samtools fastq -F 2816 -c 6 -1 {params.local_tmp}/{sample}_1.fastq.gz -2 {params.local_tmp}/{sample}_2.fastq.gz {params.local_tmp}/{sample}.collated.bam
+		cp {params.local_tmp}/{sample}_1.fastq.gz {output.fq1}
+		cp {params.local_tmp}/{sample}_2.fastq.gz {output.fq2}
+		rm -r {params.local_tmp}
+		"""
+
+#Make sure that all final output files get created
+rule make_all:
+	input:
+		expand("processed/{{dataset}}/fastq/{sample}.fastq.gz", sample=config["samples"])
+	output:
+		"processed/{dataset}/out.txt"
+	resources:
+		mem = 100
+	threads: 1
+	shell:
+		"echo 'Done' > {output}"
--- run.sh
+++ run.sh
@@ -1,1 +1,20 @@
-snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription.snakefile processed/GEUVADIS/STAR/ERR188307/ERR188307.Aligned.sortedByCoord.out.bam --jobs 1 --configfile configs/GEUVADIS_config.yaml
+snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription.snakefile processed/GEUVADIS/STAR/ERR188374/ERR188374.Aligned.sortedByCoord.out.bam --jobs 1 --configfile configs/GEUVADIS_config.yaml
+
+snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription.snakefile processed/GEUVADIS/bigwig/ERR188307.str1.bw --jobs 1 --configfile configs/GEUVADIS_config.yaml
+
+
+"processed/{study}/salmon/{annotation}/{sample}/quant.sf"
+
+
+snakemake --cluster scripts/snakemake_submit_UT.py -p -s quantify_transcription.snakefile processed/GEUVADIS/out.txt --jobs 100 --configfile configs/GEUVADIS_config.yaml --rerun-incomplete
+
+snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription.snakefile processed/GEUVADIS/out.txt --jobs 100 --configfile configs/GEUVADIS_config.yaml --rerun-incomplete
+
+
+rm /tmp/*.fifo*
+
+
+#### TwinsUK dataset ####
+#Convert bams to fastq
+snakemake --cluster scripts/snakemake_submit_UT.py -np -s bam_to_fastq_PE.snakefile processed/TwinsUK/out.txt --jobs 1 --configfile configs/TwinsUK_config.yaml --rerun-incomplete
+
