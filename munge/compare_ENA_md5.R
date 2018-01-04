449780b8f56b193d0659c366fcad54d3d10c36c5
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-01-04 17:33:49 +0200

    Check PU.1 bam files

--- munge/compare_ENA_md5.R
+++ munge/compare_ENA_md5.R
@@ -30,3 +30,27 @@ md5_df = dplyr::bind_rows(dplyr::transmute(md5, md5 = md5_1, fastq = fq1),
 md5_loc = read.table("metadata/CTCF/CTCF_md5.txt", stringsAsFactors = FALSE, col.names = c("md5", "file_name")) %>%
   tbl_df()
 dplyr::anti_join(md5_df, md5_loc, by = c("file_name", "md5"))
+
+
+#### PU.1 study ####
+md5 = readr::read_tsv("../Blood_ATAC/Blood_ATAC/data/Waszak_2015/E-MTAB-3657.sdrf.txt")
+metadata = md5[,c(1,6,22,23,24)]
+colnames(metadata) = c("sample_id", "genotype_id", "URL", "md5", "phenotype")
+
+clean_metadata = dplyr::mutate(metadata, file_name = strsplit(URL, "/") %>% 
+                                 purrr::map_chr(., tail, 1) %>% unlist()) %>%
+  dplyr::select(sample_id, genotype_id, phenotype, file_name, md5, URL) %>%
+  dplyr::mutate(phenotype = ifelse(phenotype == "PU.1", "PU1", phenotype)) %>% 
+  dplyr::mutate(sample_id = paste(genotype_id, phenotype, sep = "_"))
+write.table(clean_metadata, "../Blood_ATAC/Blood_ATAC/data/Waszak_2015/Waszak_2015_clean_metadata.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+#Extract PU.1 samples
+pu1_data = dplyr::filter(clean_metadata, phenotype == "PU1")
+
+#Compare md5 values
+md5_loc = read.table("../Blood_ATAC/Blood_ATAC/data/Waszak_2015/PU1_md5.txt", stringsAsFactors = FALSE, col.names = c("md5", "file_name")) %>%
+  tbl_df()
+dplyr::anti_join(pu1_data, md5_loc, by = c("file_name", "md5"))
+
+pu1_data
+
