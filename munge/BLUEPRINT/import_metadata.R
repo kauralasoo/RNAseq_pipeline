4b3b36376b5b60c2bd92dcc7b021f35f93d25cec
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-05-19 01:43:07 +0300

    Identify missing fastq files

--- munge/BLUEPRINT/import_metadata.R
+++ munge/BLUEPRINT/import_metadata.R
@@ -3,8 +3,8 @@ library("readr")
 library("data.table")
 
 #Sample files
-sample_files = readr::read_tsv("metadata/BLUEPRINT/monocyte_sample_file.map", col_names = c("sample_id","id1", "file_name", "id2")) %>%
-  dplyr::select(-id1, -id2) %>%
+sample_files = readr::read_tsv("metadata/BLUEPRINT/monocyte_sample_file.map", col_names = c("sample_id","id1", "file_name", "ega_file")) %>%
+  dplyr::select(-id1) %>%
   dplyr::filter(!(file_name %like% ".bam.")) %>%
   tidyr::separate(file_name, c("file_name", "suffix"), ".cip") %>%
   dplyr::select(-suffix) %>%
@@ -15,6 +15,14 @@ file_names = readr::read_tsv("metadata/BLUEPRINT/monocyte_fastq_files.txt", col_
   tidyr::separate(fastq, c("prefix","ega_id", "file_name"), "_", extra = "merge", remove = FALSE) %>%
   dplyr::select(-prefix)
 
-sample_names = dplyr::left_join(sample_files, file_names, by = "file_name") %>% dplyr::select(sample_id, ega_id, fastq) %>%
+sample_names = dplyr::left_join(sample_files, file_names, by = "file_name") %>% dplyr::select(sample_id, ega_id, fastq, ega_file) %>%
   dplyr::filter(!is.na(fastq))
 write.table(sample_names, "metadata/BLUEPRINT/monocyte_compiled_metadata.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+
+
+#Identify missing fastq files from EGA
+all_files = read.table("metadata/BLUEPRINT/monocyte_EGA_list.txt", stringsAsFactors = FALSE)
+setdiff(all_files$V3, sample_names$ega_file)
+
+
+
