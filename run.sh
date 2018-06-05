be85fb3bdfec79945c30433f9ec2fa6cdf7826b9
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-06-05 17:56:33 +0300

    Update GEUVADIS config

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 #Run the quantification pipeline
 snakemake --cluster scripts/snakemake_submit_UT.py -p -s quantify_transcription.snakefile processed/GEUVADIS/out.txt --jobs 100 --configfile configs/GEUVADIS_config.yaml --rerun-incomplete
 
-snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription.snakefile processed/GEUVADIS/out.txt --jobs 100 --configfile configs/GEUVADIS_config.yaml --rerun-incomplete
-
 
 #### TwinsUK dataset ####
 #Convert bams to fastq
@@ -13,11 +11,8 @@ snakemake --cluster scripts/snakemake_submit_UT.py -np -s bam_to_fastq_PE.snakef
 #Run the alignment pipeline
 snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription.snakefile processed/TwinsUK/out.txt --jobs 100 --configfile configs/TwinsUK_config.yaml --rerun-incomplete
 
-snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription.snakefile processed/TwinsUK/out.txt --jobs 100 --configfile configs/TwinsUK_config.yaml --rerun-incomplete
 
-
-
-### Nedelec 2016 dataset ####
+#### Nedelec 2016 dataset ####
 #Convert .sra files to fastq
 snakemake --cluster scripts/snakemake_submit_UT.py -np -s sra_to_fastq.snakefile processed/Macrophages_Nedelec_2016/out.txt --jobs 1 --configfile configs/Nedelec_config.yaml --rerun-incomplete
 
@@ -25,14 +20,19 @@ snakemake --cluster scripts/snakemake_submit_UT.py -np -s sra_to_fastq.snakefile
 snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription_SE.snakefile processed/Macrophages_Nedelec_2016/out.txt --jobs 1 --configfile configs/Nedelec_config.yaml --rerun-incomplete
 
 
-### Quach 2016 dataset ####
+#### Quach 2016 dataset ####
 #Quantify transcription
 snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription_SE.snakefile processed/Monocytes_Quach_2016/out.txt --jobs 1 --configfile configs/Quach_2016_config.yaml --rerun-incomplete
 
-### BLUEPRINT dataset ####
+
+#### BLUEPRINT dataset ####
 #Quantify transcription
 snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription_SE.snakefile processed/BLUEPRINT/out.txt --jobs 1 --configfile configs/BLUEPRINT_SE_config.yaml --rerun-incomplete
 snakemake --cluster scripts/snakemake_submit_UT.py -p -s quantify_transcription_PE.snakefile processed/BLUEPRINT/out.txt --jobs 20 --configfile configs/BLUEPRINT_PE_config.yaml --rerun-incomplete
 
-### Alasoo et al ####
+
+#### Alasoo et al ####
 snakemake --cluster scripts/snakemake_submit_UT.py -np -s quantify_transcription_PE.snakefile processed/Macrophages_Alasoo_2018/out.txt --jobs 20 --configfile configs/Alasoo_2018_config.yaml --rerun-incomplete
+
+#### Fairfax et al ####
+snakemake --cluster scripts/snakemake_submit_UT.py -p -s quantify_transcription_SE.snakefile processed/Fairfax/out.txt --jobs 1 --configfile configs/Fairfax_config.yaml --rerun-incomplete
