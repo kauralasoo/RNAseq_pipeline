552363287c30a954b02801da867eb38f5f97ecea
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-07-27 00:53:15 +0300

    use purrr::reduce to merge SEs

--- QC/make_joint_PCA.R
+++ QC/make_joint_PCA.R
@@ -10,28 +10,24 @@ library("ggplot2")
 library("data.table")
 
 
-BLUEPRINT = readRDS("results/SummarizedExperiments/BLUEPRINT.rds")
-Quach = readRDS("results/SummarizedExperiments/Quach_2016_Monocytes.rds")
-Nedelec = readRDS("results/SummarizedExperiments/Nedelec_2016_Macrophages.rds")
-Alasoo = readRDS("results/SummarizedExperiments/Alasoo_2018_Macrophages.rds")
-GEUVADIS = readRDS("results/SummarizedExperiments/GEUVADIS.rds")
-TwinsUK = readRDS("results/SummarizedExperiments/TwinsUK.rds")
+se_list = list(BLUEPRINT = readRDS("results/SummarizedExperiments/BLUEPRINT.rds"),
+     Quach = readRDS("results/SummarizedExperiments/Quach_2016_Monocytes.rds"),
+     Nedelec = readRDS("results/SummarizedExperiments/Nedelec_2016_Macrophages.rds"),
+     Alasoo = readRDS("results/SummarizedExperiments/Alasoo_2018_Macrophages.rds"),
+     GEUVADIS = readRDS("results/SummarizedExperiments/GEUVADIS.rds"),
+     TwinsUK = readRDS("results/SummarizedExperiments/TwinsUK.rds"))
 
 
 #Merge studies
-merged_data = mergeCountsSEs(BLUEPRINT, Quach) %>%
-  mergeCountsSEs(Nedelec) %>%
-  mergeCountsSEs(Alasoo) %>%
-  mergeCountsSEs(GEUVADIS) %>%
-  mergeCountsSEs(TwinsUK)
-
+merged_data = purrr::reduce(se_list, mergeCountsSEs)
 
 #Keep only naive samples
 merged_data = merged_data[,merged_data$condition == "naive"]
 
-
 processed_se = filterSE_gene_types(merged_data, valid_gene_types = "protein_coding") %>% normaliseSE_tpm()
 processed_se = processed_se[apply(assays(processed_se)$tpms, 1, median) > 1,]
+
+#Make PCA
 pca_res = transformSE_PCA(processed_se, assay_name = "tpms", n_pcs = 10, log_transform = TRUE, center = TRUE, scale. = TRUE)
 ggplot(pca_res$pca_matrix, aes(x = PC1, y = PC2, color = cell_type, shape = study)) + geom_point()
 
@@ -58,4 +54,22 @@ processed_se = filterSE_gene_types(merged_data, valid_gene_types = "protein_codi
 processed_se = processed_se[apply(assays(processed_se)$tpms, 1, median) > 1,]
 pca_res = transformSE_PCA(processed_se, assay_name = "tpms", n_pcs = 10, log_transform = TRUE, center = TRUE, scale. = TRUE)
 ggplot(pca_res$pca_matrix, aes(x = PC1, y = PC2, color = condition, shape = study)) + geom_point()
+
+
+
+#Count donors and samples
+coldata = colData(merged_data) %>% tbl_df2()
+
+n_samples = dplyr::select(coldata, study, sample_id) %>% 
+  dplyr::distinct() %>% dplyr::group_by(study) %>% 
+  dplyr::summarise(n_samples = length(sample_id))
+
+n_donors = dplyr::select(coldata, study, genotype_id) %>% 
+  dplyr::distinct() %>% dplyr::group_by(study) %>% 
+  dplyr::summarise(n_donors = length(genotype_id))
+
+counts = dplyr::left_join(n_samples, n_donors, by = "study")
+write.table(counts, "results/counts.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+
 
