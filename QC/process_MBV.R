13ca8961c522338471d2061dfa9220baf9c361e2
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-10-01 10:07:08 +0300

    Process mbv results for GENCORD

--- QC/process_MBV.R
+++ QC/process_MBV.R
@@ -63,3 +63,22 @@ res = dplyr::transmute(a, mbv_genotype_id = SampleID,
                        het_consistent_frac = n_het_consistent/n_het_covered, 
                        hom_consistent_frac = n_hom_consistent/n_hom_covered)
 ggplot(res, aes(x = het_consistent_frac, y = hom_consistent_frac, label = mbv_genotype_id)) + geom_point() + geom_text()
+
+
+
+#### GENCORD ####
+#Get file names
+mbv_files = list.files("processed/GENCORD/mbv/", full.names = T)
+
+#Make sample names
+sample_names = stringr::str_replace_all(basename(mbv_files), ".mbv_output.txt", "")
+sample_list = setNames(mbv_files, sample_names)
+
+#Import mbv files
+mbv_results = purrr::map(sample_list, ~readr::read_delim(., delim = " ", col_types = "ciiiiiiiiii"))
+
+#Find best matches
+best_matches = purrr::map_df(mbv_results, mbvFindBestMatch, .id = "sample_id") %>%
+  dplyr::filter(!is.na(het_consistent_frac))
+write.table(best_matches, "metadata/GEUVADIS/GEUVADIS_mbv_best_match.txt", sep = "\t", quote = F, row.names = F)
+
