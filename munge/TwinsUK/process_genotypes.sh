dc19d12238793cc02b9552e2d6f1957667493ee2
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-10-03 01:25:18 +0300

    Filter unrelated individuals

--- munge/TwinsUK/process_genotypes.sh
+++ munge/TwinsUK/process_genotypes.sh
@@ -1,4 +1,5 @@
 # Add INFO scores and standard variant IDs
 bcftools +impute-info TwinsUK_GRCh38.vcf.gz | bcftools annotate --set-id 'chr%CHROM\_%POS\_%REF\_%FIRST_ALT' -Oz -o TwinsUK_GRCh38.annotated.vcf.gz
 
-bcftools filter -i 'MAF[0] > 0.05' TwinsUK_GRCh38.annotated.vcf.gz -Oz -o TwinsUK_GRCh38.filtered.vcf.gz
+#Keep only unrelated individuals
+bcftools view -S  ~/rocket/projects/GEUVADIS/RNAseq_pipeline/metadata/TwinsUK/TwinsUK_unrelated_individuals.txt TwinsUK_GRCh38.annotated.vcf.gz | bcftools filter -i 'MAF[0] > 0.05' -Oz -o TwinsUK_GRCh38.filtered.vcf.gz
