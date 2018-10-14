28d97dc92cbe08496d3152c8b095f7bc00411df1
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-10-14 20:15:41 +0300

    BLUEPRINT MBV

--- QC/process_MBV.R
+++ QC/process_MBV.R
@@ -56,11 +56,11 @@ mbv_results = purrr::map(sample_list, ~readr::read_delim(., delim = " ", col_typ
 #Find best matches
 best_matches = purrr::map_df(mbv_results, mbvFindBestMatch, .id = "sample_id") %>%
   dplyr::filter(!is.na(het_consistent_frac)) %>%
-  dplyr::filter(het_consistent_frac > 0.9)
-write.table(best_matches, "../Fairfax_monocytes/data/metadata/Fairfax_mbv_best_match.txt", sep = "\t", quote = F, row.names = F)
+  dplyr::filter(hom_min_dist > 0.2)
+write.table(best_matches, "metadata/BLUEPRINT/BLUEPRINT_mbv_best_match.txt", sep = "\t", quote = F, row.names = F)
 
 
-a = mbv_results$S003Q3B1_mRNA
+a = mbv_results$S003P5B1_RNA
 res = dplyr::transmute(a, mbv_genotype_id = SampleID, 
                        het_consistent_frac = n_het_consistent/n_het_covered, 
                        hom_consistent_frac = n_hom_consistent/n_hom_covered)
