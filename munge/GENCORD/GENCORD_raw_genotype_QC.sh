6e6cb161a1caf8f69be5d97bc3a65b0f3485a9de
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-01-17 00:55:52 +0200

    Extract variant info

--- munge/GENCORD/GENCORD_raw_genotype_QC.sh
+++ munge/GENCORD/GENCORD_raw_genotype_QC.sh
@@ -86,29 +86,33 @@ bcftools view -r 22 GENCORD_GRCh37_genotyped.vcf.gz -Oz -o by_chr/GENCORD_GRCh37
 bcftools view -r X GENCORD_GRCh37_genotyped.vcf.gz -Oz -o by_chr/GENCORD_GRCh37_chrX.vcf.gz
 
 #Extraxt all inputed files
-7za x chr_1.zip -p'Thr6tAkSFIQfU2'
-7za x chr_2.zip -p'Thr6tAkSFIQfU2'
-7za x chr_3.zip -p'Thr6tAkSFIQfU2'
-7za x chr_4.zip -p'Thr6tAkSFIQfU2'
-7za x chr_5.zip -p'Thr6tAkSFIQfU2'
-7za x chr_6.zip -p'Thr6tAkSFIQfU2'
-7za x chr_7.zip -p'Thr6tAkSFIQfU2'
-7za x chr_8.zip -p'Thr6tAkSFIQfU2'
-7za x chr_9.zip -p'Thr6tAkSFIQfU2'
-7za x chr_10.zip -p'Thr6tAkSFIQfU2'
-7za x chr_11.zip -p'Thr6tAkSFIQfU2'
-7za x chr_12.zip -p'Thr6tAkSFIQfU2'
-7za x chr_13.zip -p'Thr6tAkSFIQfU2'
-7za x chr_14.zip -p'Thr6tAkSFIQfU2'
-7za x chr_15.zip -p'Thr6tAkSFIQfU2'
-7za x chr_16.zip -p'Thr6tAkSFIQfU2'
-7za x chr_17.zip -p'Thr6tAkSFIQfU2'
-7za x chr_18.zip -p'Thr6tAkSFIQfU2'
-7za x chr_19.zip -p'Thr6tAkSFIQfU2'
-7za x chr_20.zip -p'Thr6tAkSFIQfU2'
-7za x chr_21.zip -p'Thr6tAkSFIQfU2'
-7za x chr_22.zip -p'Thr6tAkSFIQfU2'
+7za x chr_1.zip -p'password'
+7za x chr_2.zip -p'password'
+7za x chr_3.zip -p'password'
+7za x chr_4.zip -p'password'
+7za x chr_5.zip -p'password'
+7za x chr_6.zip -p'password'
+7za x chr_7.zip -p'password'
+7za x chr_8.zip -p'password'
+7za x chr_9.zip -p'password'
+7za x chr_10.zip -p'password'
+7za x chr_11.zip -p'password'
+7za x chr_12.zip -p'password'
+7za x chr_13.zip -p'password'
+7za x chr_14.zip -p'password'
+7za x chr_15.zip -p'password'
+7za x chr_16.zip -p'password'
+7za x chr_17.zip -p'password'
+7za x chr_18.zip -p'password'
+7za x chr_19.zip -p'password'
+7za x chr_20.zip -p'password'
+7za x chr_21.zip -p'password'
+7za x chr_22.zip -p'password'
 
 #Filter final vcf file and add unique variant ids
 bcftools filter -i 'MAF[0] > 0.01' GENCORD_GRCh38.vcf.gz | bcftools annotate --set-id 'chr%CHROM\_%POS\_%REF\_%FIRST_ALT' -Oz -o GENCORD_GRCh38.filtered.vcf.gz
 bcftools index GENCORD_GRCh38.filtered.vcf.gz
+
+#Extract variant information
+module load bcftools-1.8
+bcftools +fill-tags GENCORD_GRCh38.filtered.vcf.gz | bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t%TYPE\t%AC\t%AN\t%MAF\t%R2\n' | gzip > GENCORD_GRCh38.variant_information.txt.gz
