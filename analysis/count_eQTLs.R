4fa05c4ecd549717d127f99f14d10039d9bb9265
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-01-27 21:33:33 +0200

    Count eQTLs

--- analysis/count_eQTLs.R
+++ analysis/count_eQTLs.R
@@ -12,7 +12,7 @@ lead_var_list = purrr::map(lead_vars, ~eQTLUtils::importQTLtoolsTable(.))
 #Count QTLs
 qtl_count = purrr::map_df(lead_var_list, ~dplyr::filter(., p_fdr < 0.05) %>% dplyr::summarise(eQTL_count = length(phenotype_id)), .id = "path")
 feature_count = purrr::map_df(lead_var_list, ~dplyr::summarise(.,feature_count = length(phenotype_id)), .id = "path")
-snp_count = purrr::map_df(lead_var_list, ~dplyr::summarise(.,mean_snp_count = mean(n_cis_snps), sd_snp_count = sd(n_cis_snps)), .id = "path")
+snp_count = purrr::map_df(lead_var_list, ~dplyr::summarise(.,mean_snp_count = mean(n_cis_snps), sd_snp_count = sd(n_cis_snps), median_snp_count = median(n_cis_snps)), .id = "path")
 
 qtl_counts = dplyr::left_join(qtl_count, feature_count, by = "path") %>% 
   dplyr::left_join(snp_count, by = "path") %>%
@@ -25,7 +25,16 @@ meta_list = list(BLUEPRINT = "metadata/cleaned/BLUEPRINT.tsv",
                  CEDAR = "metadata/cleaned/CEDAR.tsv",
                  Fairfax_2014 = "metadata/cleaned/Fairfax_2014.tsv",
                  GENCORD = "metadata/cleaned/GENCORD.tsv",
-                 GEUVADIS = "metadata/cleaned/GEUVADIS.tsv")
+                 GEUVADIS = "metadata/cleaned/GEUVADIS.tsv",
+                 TwinsUK = "metadata/cleaned/TwinsUK.tsv",
+                 Alasoo_2018 = "metadata/cleaned/Alasoo_2018.tsv",
+                 Nedelec_2016 = "metadata/cleaned/Nedelec_2016.tsv",
+                 Quach_2016 = "metadata/cleaned/Quach_2016.tsv",
+                 Fairfax_2012 = "metadata/cleaned/Fairfax_2012.tsv",
+                 Schwartzentruber_2018 = "metadata/cleaned/Schwartzentruber_2018.tsv",
+                 van_de_Bunt_2015 = "metadata/cleaned/van_de_Bunt_2015.tsv",
+                 HipSci = "metadata/cleaned/HipSci.tsv",
+                 Naranbhai_2015 = "metadata/cleaned/Naranbhai_2015.tsv")
 meta_imported = purrr::map(meta_list, ~read.table(., sep = "\t", stringsAsFactors = F, header =T) %>% dplyr::as_tibble())
 samples = purrr::map_df(meta_imported, ~dplyr::select(.,cell_type, condition, qtl_group, rna_qc_passed, genotype_qc_passed, study))
 cell_types = dplyr::select(samples, cell_type, condition, qtl_group, study) %>% dplyr::distinct()
@@ -37,16 +46,20 @@ sample_sizes = dplyr::filter(samples, rna_qc_passed, genotype_qc_passed) %>%
 #Merge counts
 merged_counts = dplyr::left_join(qtl_counts, sample_sizes, by = c("study", "qtl_group")) %>% 
   dplyr::left_join(cell_types, by = c("qtl_group", "study"))
-ggplot(merged_counts, aes(x = sample_size, y = eQTL_count, color = study, label = cell_type)) + 
+eGene_scatter = ggplot(merged_counts, aes(x = sample_size, y = eQTL_count, color = study, label = cell_type)) + 
   geom_point() +
   scale_y_continuous(limits = c(0, 7500)) +
   scale_x_continuous(limits = c(0, 460)) +
   ylab("Number of eGenes") +
-  xlab("sample size")
+  xlab("Sample size")
+ggsave("results/figures/eGenes_samples_scatter.pdf", plot = eGene_scatter, width = 7, height = 5)
 
 #Visualise SNP counts per study
 snp_counts_df = dplyr::select(qtl_counts, study, mean_snp_count, sd_snp_count) %>% dplyr::distinct()
-ggplot(snp_counts_df, aes(x = study, y = mean_snp_count, ymax = mean_snp_count+sd_snp_count, ymin = mean_snp_count-sd_snp_count, fill = study)) + 
+snp_count = ggplot(snp_counts_df, aes(x = study, y = mean_snp_count, ymax = mean_snp_count+sd_snp_count, ymin = mean_snp_count-sd_snp_count, fill = study)) + 
   geom_bar(stat = "identity") + 
-  geom_errorbar(width = 0.2)
+  geom_errorbar(width = 0.2) + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("results/figures/snp_count_per_gene.pdf", plot = snp_count, width = 8, height = 5)
+
 
--- import_export/array_data_to_QTLtools.R
+++ import_export/array_data_to_QTLtools.R
@@ -11,7 +11,7 @@ valid_chromosomes = c("1","10","11","12","13","14","15","16","17","18","19",
 #Fairfax 2014
 gene_meta = readr::read_tsv("metadata/gene_metadata/HumanHT-12_V4_gene_metadata.txt.gz")
 sample_metadata = readr::read_tsv("metadata/cleaned/Fairfax_2014.tsv")
-expression_matrix = read.table("results/expression_matrices/HumanHT-12_V4/Fairfax_2014.tsv.gz", sep = "\t")
+expression_matrix = read.table("results/expression_matrices/HumanHT-12_V4/Fairfax_2014.tsv.gz", sep = "\t", check.names = FALSE)
 fairfax_2014_se = makeSummarizedExperiment(expression_matrix, gene_meta, sample_metadata, assay_name = "exprs")
 
 #Filter SE to keep correct chromosomes and QC-passed samples
@@ -63,7 +63,7 @@ expression_matrix = read.table("results/expression_matrices/HumanHT-12_V4/CEDAR.
 cedar_se = makeSummarizedExperiment(expression_matrix, gene_meta, sample_metadata, assay_name = "exprs")
 
 #Filter SE to keep correct chromosomes and QC-passed samples
-se_filtered = filterSummarizedExperiment(cedar_se, valid_chromosomes = valid_chromosomes, filter_rna_qc = TRUE, filter_genotype_qc = TRUE)
+se_filtered = filterSummarizedExperiment(cedar_se, valid_chromosomes = valid_chromosomes, filter_rna_qc = F, filter_genotype_qc = F)
 
 #Normalize and regress out batch effects
 se_norm = array_normaliseSE(se_filtered, norm_method = "quantile", assay_name = "exprs", log_transform = TRUE, adjust_batch = TRUE, filter_quality = TRUE)
@@ -78,4 +78,25 @@ studySEtoQTLTools(se_norm_filtered, assay_name = "norm_exprs", "processed/CEDAR/
 #Perform PCA
 pca = eQTLUtils::transformSE_PCA(se_norm, assay_name = "norm_exprs")
 ggplot(pca$pca_matrix, aes(x = PC3, y = PC4, color = cell_type)) + geom_point()
+
+
+
+
+#BLUEPRINT
+
+
+gene_meta = read.table("metadata/gene_metadata/featureCounts_Ensembl_92_gene_metadata.txt.gz", header = T, stringsAsFactors = F) %>% dplyr::as_tibble()
+sample_metadata = readr::read_tsv("metadata/cleaned/GENCORD.tsv")
+expression_matrix = read.table("results/expression_matrices/featureCounts/GENCORD.tsv.gz", sep = "\t")
+featureCounts_se = makeSummarizedExperiment(expression_matrix, gene_meta, sample_metadata, assay_name = "counts")
+
+#Filter and normalise data for QTL mapping
+qtltools_se = qtltools_prepareFeatureCountsData(featureCounts_se)
+
+#Count the number of variants proximal to each gene and remove genes without variants
+var_info = importVariantInformation("results/var_info/CEDAR_GRCh38.variant_information.txt.gz")
+se_norm_filtered = checkCisVariants(se_norm, var_info)
+
+
+
 
