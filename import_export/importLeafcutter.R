639dcda4aef2caf275155b86585de1fc8c65b65e
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-03-13 01:53:25 +0200

    Import Leafcutter quants from GEUVADIS

--- import_export/importLeafcutter.R
+++ import_export/importLeafcutter.R
@@ -14,15 +14,21 @@ leafcutter_meta = leafcutterAnnotateIntrons(rownames(geuvadis_lc),
         transcript_meta)
 
 #Import sample metadata
-geuvadis_meta = read.table("metadata/cleaned/GEUVADIS.tsv", stringsAsFactors = F, header = T) %>% 
+geuvadis_meta = read.table("../SampleArcheology/studies/cleaned/GEUVADIS_EUR.tsv", stringsAsFactors = F, header = T) %>% 
   dplyr::as_tibble()
 
+#Make Summarized experiment
 geuvadis_lc_se = makeSummarizedExperiment(geuvadis_lc, leafcutter_meta, geuvadis_meta, assay_name = "counts")
 
+#Normalise
+geuvadis_norm = qtltoolsPrepareSE(geuvadis_lc_se[,1:10], quant_method = "LeafCutter")
 
+#Export Leafcutter gene metadata file
+write.table(leafcutter_meta, "metadata/gene_metadata/GEUVADIS_leafcutter_cluster_metadata.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
+#Write count matrix as tab-separated file
+gz1 = gzfile("results/expression_matrices/LeafCutter/GEUVADIS.tsv.gz","w") 
+write.table(geuvadis_lc, gz1, sep = "\t", quote = F)
+close(gz1)
 
   
-
-
-
