bcc5af6633535e5dfea0af7351bd5089393d6949
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-09-19 00:38:14 +0300

    Update basic_qtl_qc.R

--- QC/basic_qtl_qc.R
+++ QC/basic_qtl_qc.R
@@ -71,6 +71,11 @@ dplyr::filter(naive_txrevise_qtls, group_id %like% "ENSG00000113161.contained")$
 
 dplyr::filter(naive_txrevise_qtls, group_id %like% "ENSG00000105383.contained")$p_fdr
 
+#Identify genes with at least one QTL
+naive_qtls = importQTLtoolsTable("results/finemapping/Alasoo_2018_permuted/final/Alasoo_2018_ge_macrophage_naive/Alasoo_2018_ge_macrophage_naive.permuted.txt.gz") %>%
+  dplyr::filter(p_fdr < 0.1)
+write.table(naive_qtls$phenotype_id, "results/finemapping/Alasoo_2018/naive_qtl_gene_list.txt", sep = "\t", quote = F, row.names = F, col.names = F)
+
 
 
 
