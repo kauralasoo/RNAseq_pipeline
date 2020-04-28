f3191f7327b586f91589731e79c0a9a559b8a14d
Kaur Alasoo <kaur.alasoo@gmail.com>
2020-04-28 21:49:44 +0300

    Normalise RNA-seq datasets

--- run_history/run_normalisation.sh
+++ run_history/run_normalisation.sh
@@ -1,177 +1,49 @@
-##### GENCORD ####
-#gene_counts
-singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/GENCORD/featureCounts/merged_gene_counts.txt\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GENCORD.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/gene_counts\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
+### CEDAR ###
+./nextflow run normalisation.nf -profile tartu_hpc \
+    -resume \
+    --study_name CEDAR\
+    --is_microarray\
+    --microarray_exp_matrix_path /gpfs/hpc/projects/eQTLCatalogue/processed/expression_matrices/HumanHT-12_V4/raw/CEDAR.tsv.gz\
+    --sample_meta_path /gpfs/hpc/projects/eQTLCatalogue/SampleArcheology/studies/cleaned/CEDAR.tsv\
+    --vcf_file /gpfs/hpc/projects/genomic_references/CEDAR/genotypes/Michigan_GRCh37_1KGPhase3_220918/GRCh38/CEDAR_GRCh38.filtered.renamed.vcf.gz\
+    --outdir HumanHT-12_V4
 
- #transcript_usage
-singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/GENCORD/Salmon/merged_counts/gencode.v30.transcripts/gencode.v30.transcripts.TPM.merged.txt\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GENCORD.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/transcript_usage_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/transcript_usage/\
- -q transcript_usage\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
+### Fairfax_2012 ###
+./nextflow run normalisation.nf -profile tartu_hpc \
+    -resume \
+    --study_name Fairfax_2012\
+    --is_microarray\
+    --microarray_exp_matrix_path /gpfs/hpc/projects/eQTLCatalogue/processed/expression_matrices/HumanHT-12_V4/raw/Fairfax_2012.tsv.gz\
+    --sample_meta_path /gpfs/hpc/projects/eQTLCatalogue/SampleArcheology/studies/cleaned/Fairfax_2012.tsv\
+    --vcf_file /gpfs/hpc/projects/Fairfax_2014/genotypes/Michigan_GRCh37_1KGPhase3_061118/GRCh38/Fairfax_2014_GRCh38.filtered.renamed.vcf.gz\
+    --outdir HumanHT-12_V4
 
-#txrevise
-singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/GENCORD/Salmon/merged_counts/\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GENCORD.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/transcript_usage_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/txrevise/\
- -q transcript_usage\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
+### Fairfax_2014 ###
+./nextflow run normalisation.nf -profile tartu_hpc \
+    -resume \
+    --study_name Fairfax_2014\
+    --is_microarray\
+    --microarray_exp_matrix_path /gpfs/hpc/projects/eQTLCatalogue/processed/expression_matrices/HumanHT-12_V4/raw/Fairfax_2014.tsv.gz\
+    --sample_meta_path /gpfs/hpc/projects/eQTLCatalogue/SampleArcheology/studies/cleaned/Fairfax_2014.tsv\
+    --vcf_file /gpfs/hpc/projects/Fairfax_2014/genotypes/Michigan_GRCh37_1KGPhase3_061118/GRCh38/Fairfax_2014_GRCh38.filtered.renamed.vcf.gz\
+    --outdir HumanHT-12_V4
 
+### Naranbhai_2015 ###
+./nextflow run normalisation.nf -profile tartu_hpc \
+    -resume \
+    --study_name Naranbhai_2015\
+    --is_microarray\
+    --microarray_exp_matrix_path /gpfs/hpc/projects/eQTLCatalogue/processed/expression_matrices/HumanHT-12_V4/raw/Naranbhai_2015.tsv.gz\
+    --sample_meta_path /gpfs/hpc/projects/eQTLCatalogue/SampleArcheology/studies/cleaned/Naranbhai_2015.tsv\
+    --vcf_file /gpfs/hpc/projects/Fairfax_2014/genotypes/Michigan_GRCh37_1KGPhase3_061118/GRCh38/Fairfax_2014_GRCh38.filtered.renamed.vcf.gz\
+    --outdir HumanHT-12_V4
 
- ##### GEUVADIS ####
- singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/GEUVADIS/featureCounts/merged_gene_counts.txt\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GEUVADIS.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/gene_counts\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-
- ##### TwinsUK ####
- singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/TwinsUK/featureCounts/merged_gene_counts.txt\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/TwinsUK.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/gene_counts/\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
- ##### Lepik_2017 ####
- #gene_counts
-singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/Lepik_2017/featureCounts/merged_gene_counts.txt\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Lepik_2017.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/gene_counts/\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-#transcript_usage
-singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/Lepik_2017/Salmon/merged_counts/gencode.v30.transcripts/gencode.v30.transcripts.TPM.merged.txt\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Lepik_2017.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/transcript_usage_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/transcript_usage/\
- -q transcript_usage\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-#exon_counts
-singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/Lepik_2017/dexseq_exon_counts/merged_exon_counts.tsv\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Lepik_2017.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/exon_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/exon_counts/\
- -q exon_counts\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-
- #### Schwartzentruber_2018 ###
- singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/Schwartzentruber_2018/Salmon/merged_counts/TPM/\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Schwartzentruber_2018.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/txrevise_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/txrevise/\
- -q txrevise\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-
-
-
-
-
-#### Microarray datasets ####
- ##### Naranbhai_2015 ####
- singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/raw/Naranbhai_2015.tsv.gz\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Naranbhai_2015.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/HumanHT-12_V4_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/\
- -q HumanHT-12_V4\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-##### Fairfax_2014 ####
- singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/raw/Fairfax_2014.tsv.gz\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Fairfax_2014.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/HumanHT-12_V4_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/\
- -q HumanHT-12_V4\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-##### Fairfax_2012 ####
- singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/raw/Fairfax_2012.tsv.gz\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Fairfax_2012.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/HumanHT-12_V4_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/\
- -q HumanHT-12_V4\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-##### Kasela_2017 ####
- singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/raw/Kasela_2017.tsv.gz\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Kasela_2017.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/HumanHT-12_V4_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpchome/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/\
- -q HumanHT-12_V4\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-##### CEDAR ####
-singularity exec qtl_norm_qc.img Rscript normalisation/normaliseCountMatrix.R\
- -c /gpfs/hpc/home/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/raw/CEDAR.tsv.gz\
- -s /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/CEDAR.tsv\
- -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/HumanHT-12_V4_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpc/home/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/\
- -q HumanHT-12_V4\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
- ##### Kolberg_2020 ####
-singularity exec qtl_norm_qc.img Rscript normalisation/normaliseCountMatrix.R\
- -c /gpfs/hpc/home/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/raw/Kolberg_2020.tsv.gz\
- -s /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Kolberg_2020.tsv\
- -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/HumanHT-12_V4_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpc/home/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/\
- -q HumanHT-12_V4\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
- ##### Kolberg_2020 vol2####
-singularity exec qtl_norm_qc.img Rscript normalisation/normaliseCountMatrix.R\
- -c /gpfs/hpc/home/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/raw/Kolberg_2020.tsv.gz\
- -s /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/Kolberg_2020/Kolberg_2020_duplicate.tsv\
- -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/HumanHT-12_V4_Ensembl_96_phenotype_metadata.tsv.gz\
- -o /gpfs/hpc/home/a72094/datasets/processed/expression_matrices/HumanHT-12_V4/vol2/\
- -q HumanHT-12_V4\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
-
-
-singularity exec qtl_norm_qc.img Rscript normaliseCountMatrix.R\
- -c /gpfs/hpchome/a72094/datasets/processed/GENCORD/featureCounts/merged_gene_counts.txt\
- -s /gpfs/hpchome/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GENCORD.tsv\
- -p /gpfs/hpchome/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o test\
- --eqtlutils ../eQTLUtils/\
- --filter_qc TRUE
-
+### Kasela_2017 ###
+./nextflow run normalisation.nf -profile tartu_hpc \
+    -resume \
+    --study_name Kasela_2017\
+    --is_microarray\
+    --microarray_exp_matrix_path /gpfs/hpc/projects/eQTLCatalogue/processed/expression_matrices/HumanHT-12_V4/raw/Kasela_2017.tsv.gz\
+    --sample_meta_path /gpfs/hpc/projects/eQTLCatalogue/SampleArcheology/studies/cleaned/Kasela_2017.tsv\
+    --vcf_file /gpfs/hpc/projects/EGCUT_eQTLs/Kasela_2017/genotypes/Michigan_GRCh37_1KGPhase3_220119/GRCh38/Kasela_2017_GRCh38.filtered.vcf.gz\
+    --outdir HumanHT-12_V4
